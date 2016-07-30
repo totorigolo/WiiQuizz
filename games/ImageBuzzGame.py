00ea6f95bbf26edad34a6a12580f28a41e8b3ee3
totorigolo <toto.rigolo@free.fr>
2016-07-30 13:38:30 +0200

    ImageGame : Correction de la demande du dossier -> les image s'affichent

--- games/ImageBuzzGame.py
+++ games/ImageBuzzGame.py
@@ -4,4 +4,4 @@ from CompleteBuzzGame import CompleteBuzzGame
 class ImageBuzzGame(CompleteBuzzGame):
 
     def __init__(self):
-        CompleteBuzzGame.__init__(self, window_title='Image Buzz Game', images_path='./games/images/')
+        CompleteBuzzGame.__init__(self, window_title='Image Buzz Game', images_path='prompt')
