0cd3b44a192654195bc4ac7e9d8fdfe297048607
bdvllrs <bdvllrs@gmail.com>
2016-08-21 10:44:22 +0200

    Ajout des textes pour template options

--- helpers/example_template.py
+++ helpers/example_template.py
@@ -28,22 +28,28 @@ win.new_color((255, 162, 0), 'team4')
 win.new_color('white')
 win.new_color('black')
 
-# Valeurs des points
+# Valeurs des points (templates num_players)
 win.new_text('150', 'title', 'team2', label='team1_result')
 win.new_text('150', 'title', 'team1', label='team2_result')
 win.new_text('150', 'title', 'team4', label='team3_result')
 win.new_text('150', 'title', 'team3', label='team4_result')
 
-# Texte d'équipe
+# Texte d'équipe (templates buzz_teams)
 win.new_text('Pastèque', 'very_big', 'team2', label='text_buzz_team1')
 win.new_text('Ananas', 'very_big', 'team1', label='text_buzz_team2')
 win.new_text('Kiwi', 'very_big', 'team4', label='text_buzz_team3')
 win.new_text('Melon', 'very_big', 'team3', label='text_buzz_team4')
 
+# Texte réponse (template options_game)
+win.new_text('Réponse haut', 'font', 'dark_blue', label='option_up_text')
+win.new_text('Réponse gauche', 'font', 'dark_blue', label='option_left_text')
+win.new_text('Réponse bas', 'font', 'dark_blue', label='option_down_text')
+win.new_text('Réponse droite', 'font', 'dark_blue', label='option_right_text')
+
 win.import_template('four_players')
 win.import_template('options_game')
 
-win.edit_color('option_2', 'team1')
+win.edit_color('option_left', 'team1')
 
 win.refresh()
 
