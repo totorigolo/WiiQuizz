ceb152da8d530986f5ec102515cc3e8a2f31a2e1
totorigolo <toto.rigolo@free.fr>
2016-08-22 18:36:19 +0200

    Team : id plutôt que name

--- Team.py
+++ Team.py
@@ -2,8 +2,8 @@
 
 
 class Team:
-    def _init__(self, name, wiimote, team_name):
-        self.name = name
+    def _init__(self, id, wiimote, team_name):
+        self.id = id
         self.team_name = team_name
         self.wiimote = wiimote
         self.points = 0
